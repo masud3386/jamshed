91a4e8ccdc186d3cff74d5e42eaa5e6c986e7d5a
masud3386 <masud3386@yahoo.com>
2023-03-08 19:17:34 -0500

    sujdfdklfjdlfkj

---
+++ src/lattiy/JutaDee.java
@@ -0,0 +1,22 @@
+package lattiy;
+
+public class JutaDee {
+	
+	int age ;
+	String name;
+	void getValue()
+	{
+	System.out.println(age);
+	System.out.println(name);
+	}
+
+
+	public static void main(String[] args) {
+		
+		JutaDee jd = new JutaDee();
+		System.out.println(jd.age = 56);
+		System.out.println(jd.name ="batajuta");
+		
+	}
+
+}
